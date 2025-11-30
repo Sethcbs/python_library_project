0aa33cd98f60060d2e48fc1c5192d0fb6eb80777
Sethcbs <sethmc22@gmail.com>
2025-11-30 11:17:03 -0800

    making sure all changes are saves

--- oo-lib-py/app/v1/catalog.py
+++ oo-lib-py/app/v1/catalog.py
@@ -12,7 +12,6 @@ class Catalog():
             raise ValueError(f"An item with key '{item.id.value}' is already in the catalog.")
 
     def get_item_details(self, item_ids):
-        lines = []
         cataloged_item = self._items[item_ids]
         detail_string = (f"ID: {cataloged_item.id.value}, Title: {cataloged_item.title}, Days Allowed: ({str(cataloged_item.days_allowed())} days)")
                 
